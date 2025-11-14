949b50b5e59eb9337c218ab509e7c2a33cd4ebb5
rehanhasyims <rehanhasyim24@gmail.com>
2025-11-13 22:59:33 -0800

    ainiweb

---
+++ aini.js
@@ -0,0 +1,15 @@
+import liquidbackgroud from
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnRWI3lX3z1xPbRKWseNoMK1V5fdJpNDnV_w&s'
+
+const app = 
+liquidbackgroud(document.getElementById('canvas'))
+
+app.loadimage('https://thumbs.dreamstime.com/b/landscape-nature-view-background-view-window-landscape-nature-view-background-view-window-wonderful-landscape-121459679.jpg')
+app.liquidPlane.material.metalness =0,75
+app.liquidPlane.material.roughness =0,75
+app.liquidPlane.unifroms.displacementScale.value = S 
+app.setRain(false)
+
+
+
+
